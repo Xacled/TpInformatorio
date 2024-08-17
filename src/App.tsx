02747c7f5aba21c94fddfcb06410e329087eeb26
Xacled <carlox7v7@gmail.com>
2024-08-17 18:05:14 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+
+import './App.css'
+import React from 'react'
+import Header from './Header'
+function App() {
+
+
+  return (
+    <>
+      <Header/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css'
+
+function Header() {
+    return (
+        <nav className='nav-bar background-nav'>
+            <div className=''>
+                <h1>Hola soy un texto .</h1>
+            </div>
+        </nav>  
+    );
+}
+
+export default Header;
+ 
