2984a2e5af8194d255b7a66ab12058df05276d80
Xacled <carlox7v7@gmail.com>
2024-08-31 16:52:33 -0300

    New-Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,49 @@
-import Header from "./components/Header";
-import QuickPicks from "./components/QuickPicks";
-import RecommendedAlbums from "./components/RecommendedAlbums";
-import SimilarArtists from "./components/SimilarArtists";
-import PlaybackBar from "./components/PlaybackBar";
+import { useState } from "react";
+import Header from "../src/components/Header/Header";
 import "./App.css";
-import ListenAgain from "./components/ListenAgain";
-import ContentLeft from "./components/ContentLeft";
+import { ListenAgain, QuickPicks, RecommendedAlbums, SimilarArtists } from "./components/Componentes";
+import PlaybackBar from "./components/PlaybackBar";
+import ContentLeft from "./components/SideBar";
+import PlaylistForm from "./components/form";
 
 function App() {
+  const [showPlaylistForm, setShowPlaylistForm] = useState(false);
+  const [newPlaylist, setNewPlaylist] = useState({ title: '', description: '', imageUrl: '' });
+  const [playlists, setPlaylists] = useState([]);
+
+  const handleNewPlaylistClick = () => {
+    setShowPlaylistForm(true);
+  };
+
+  const handleFormChange = (field, value) => {
+    setNewPlaylist((prev) => ({ ...prev, [field]: value }));
+  };
+
+  const handleAddPlaylist = () => {
+    setPlaylists([...playlists, newPlaylist]);
+    setShowPlaylistForm(false);
+  };
+
   return (
     <div className="app">
-      <Header />
-      <ContentLeft/>
+      <Header  />
+      <ContentLeft onNewPlaylistClick={handleNewPlaylistClick} playlists={playlists} />
 
       <main className="main-content">
-        <ListenAgain />
-        <SimilarArtists />
-        <QuickPicks />
-        <RecommendedAlbums />
+        {showPlaylistForm ? (
+          <PlaylistForm 
+            newPlaylist={newPlaylist}
+            onFormChange={handleFormChange}
+            onAddPlaylist={handleAddPlaylist}
+          />
+        ) : (
+          <>
+            <ListenAgain />
+            <SimilarArtists />
+            <QuickPicks />
+            <RecommendedAlbums />
+          </>
+        )}
       </main>
       <PlaybackBar />
     </div>
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,10 @@
+
+export const Layout = ({children}) =>{
+return(
+    <>
+        <div>
+            {children}
+        </div>
+    </>
+)
+}
